dc5605bccb274e0113587effefeb194a8fc0eb25
Thomas Barron <barron.tom@gmail.com>
2020-04-07 21:47:36 -0400

    MVP

---
+++ DevBuild_Week13_SQLDB_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevBuild_Week13_SQLDB_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
