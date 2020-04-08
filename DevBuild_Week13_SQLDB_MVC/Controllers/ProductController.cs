95af6f9e86df66fd206900c7fc1e6248da06e1bc
Thomas Barron <barron.tom@gmail.com>
2020-04-07 21:52:32 -0400

    MVP
    MVP

---
+++ DevBuild_Week13_SQLDB_MVC/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DevBuild_Week13_SQLDB_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DevBuild_Week13_SQLDB_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        IConfiguration ConfigRoot;
+        SqlConnection connection;
+
+        public ProductController(IConfiguration config)
+        {
+            ConfigRoot = config;
+            connection = new SqlConnection(ConfigRoot.GetConnectionString("coffeeDB"));
+        }
+
+        public IActionResult Index()
+        {
+            string queryString = "SELECT * FROM Products";
+            IEnumerable<Product> Products = connection.Query<Product>(queryString);
+            ViewData["Products"] = Products;
+            return View("ProductIndex");
+        }
+    }
+}
